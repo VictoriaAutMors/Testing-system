0e0986138cbc4d6b98c8cee33a15cd38b8f4ee4b
Timur Alikhanov <55821089+VictoriaAutMors@users.noreply.github.com>
2019-11-08 10:28:18 +0600

    Add files via upload

--- src/judge.c
+++ src/judge.c
@@ -3,43 +3,55 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
-#include  <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <err.h>
+#include <limits.h>
+#include <sys/wait.h>
 
-int main(int argc, char ** argv){
-    char name[] = "~/semestr2/Testing-system/code/user1";
-    struct dirent  * entry;
-    name[strlen(name) - 1] = '\0';
-    DIR* dir1 = opendir(name);
-
-    if(dir1 == NULL){
-        perror("No such directory");
-        exit(1);
+int main(void){
+    pid_t pid;
+    struct dirent  *entry;
+    char cwd[PATH_MAX], *path = NULL;
+    if (getcwd(cwd, PATH_MAX) == NULL) {
+        err(1, NULL);
     }
-
-    puts("HElloo\n");
+    DIR* dir1 = opendir(cwd);
+    if (dir1 == NULL) {
+        err(1, "No such directory");
+    }
     while ((entry = readdir(dir1)) != NULL) {
-
-    puts("HElloo\n");
-        if(entry->d_type == 8){
-            if(fork() == 0){
-                char * name_executed_program = malloc(sizeof(char) * strlen(entry->d_name));
-                strcpy(name_executed_program, entry->d_name);
-                name_executed_program[strlen(name_executed_program) - 1] = '\0';
-                name_executed_program[strlen(name_executed_program) - 1] = '\0';
-                char * Path = name;
-                Path = realloc(Path, 256 * sizeof(char));
-                strcat(Path, "/");
-                strcat(Path, entry->d_name);
-                char * cmd[7] = {"gcc", Path, "-o", name_executed_program, "-Wall", "-Werror", NULL};
-                execvp(cmd[0], cmd);
-                char exec_value[] = "./";
-                strcat(exec_value, name_executed_program);
-                char * cmd1[2] = {exec_value, NULL};
-                execvp(cmd1[0], 
-                       cmd1);
-                free(name_executed_program);
+        if (strstr(entry -> d_name, ".c") != NULL) {
+            if ((pid = fork()) == 0) {
+                char *name_exec_prg = (char *)malloc(sizeof(char) * (strlen(entry -> d_name) + 1));
+                if (name_exec_prg == NULL) {
+                    err(1, NULL);
+                }
+                strcpy(name_exec_prg, entry -> d_name);
+                name_exec_prg[strlen(name_exec_prg) - 1] = '\0';
+                name_exec_prg[strlen(name_exec_prg) - 1] = '\0';
+                path = (char *)realloc(path, PATH_MAX * sizeof(char));
+                if (path == NULL) {
+                    err(1, NULL);
+                }
+                strcpy(path, cwd);
+                strcat(path, "/");
+                strcat(path, entry -> d_name);
+                if (fork() == 0) {
+                    char * cmd[7] = {"gcc", path, "-o", name_exec_prg, "-Wall", "-Werror", NULL};
+                    if (execvp(cmd[0], cmd) < 0) {
+                        err(1, NULL);
+                    }
+                    return EXIT_SUCCESS;
+                }
+                wait(NULL);
+                if (execl(name_exec_prg, name_exec_prg, NULL) < 0) {
+                    err(1, NULL);
+                }
+                free(path);
+                free(name_exec_prg);
+                return EXIT_SUCCESS;
             }
         }
     }
