635ae75aa60b43fd7ffcc05f8a1adf8c4e454355
Timur Alikhanov <55821089+VictoriaAutMors@users.noreply.github.com>
2019-11-09 13:21:23 +0600

    Add files via upload

--- src/judge.c
+++ src/judge.c
@@ -10,51 +10,65 @@
 #include <limits.h>
 #include <sys/wait.h>
 
-int main(void){
-    pid_t pid;
-    struct dirent  *entry;
-    char cwd[PATH_MAX], *path = NULL;
-    if (getcwd(cwd, PATH_MAX) == NULL) {
+char *get_prg_name(char *name, char *dname)
+{
+    int len = strlen(dname);
+    name = (char *)realloc(name, len * sizeof(char));
+    if (name == NULL) {
         err(1, NULL);
     }
-    DIR* dir1 = opendir(cwd);
+    strncpy(name, dname, len - 2);
+    name[len - 1] = '\0';
+    return name;
+}
+
+char *get_exec_path(char *exec_path, char *dname)
+{
+    int len = strlen(dname);
+    exec_path = (char *)malloc((len + 10) * sizeof(char));
+    if (exec_path == NULL) {
+        err(1, NULL);
+    }
+    strcpy(exec_path, "../users/");
+    strcat(exec_path, dname);
+    return exec_path;
+}
+
+int main(void){
+    pid_t pid;
+    char *name  = NULL, *exec_path = NULL;
+    struct dirent  *entry;
+    DIR* dir1 = opendir("../users");
     if (dir1 == NULL) {
-        err(1, "No such directory");
+        err(1, NULL);
     }
     while ((entry = readdir(dir1)) != NULL) {
         if (strstr(entry -> d_name, ".c") != NULL) {
+            name = get_prg_name(name, entry -> d_name);
+            exec_path = get_exec_path(exec_path, entry -> d_name);
             if ((pid = fork()) == 0) {
-                char *name_exec_prg = (char *)malloc(sizeof(char) * (strlen(entry -> d_name) + 1));
-                if (name_exec_prg == NULL) {
-                    err(1, NULL);
-                }
-                strcpy(name_exec_prg, entry -> d_name);
-                name_exec_prg[strlen(name_exec_prg) - 1] = '\0';
-                name_exec_prg[strlen(name_exec_prg) - 1] = '\0';
-                path = (char *)realloc(path, PATH_MAX * sizeof(char));
-                if (path == NULL) {
-                    err(1, NULL);
-                }
-                strcpy(path, cwd);
-                strcat(path, "/");
-                strcat(path, entry -> d_name);
                 if (fork() == 0) {
-                    char * cmd[7] = {"gcc", path, "-o", name_exec_prg, "-Wall", "-Werror", NULL};
+                    char * cmd[7] = {"gcc", exec_path, "-o", name, "-Wall", "-Werror", NULL};
                     if (execvp(cmd[0], cmd) < 0) {
                         err(1, NULL);
                     }
+                    free(exec_path);
+                    free(name);
                     return EXIT_SUCCESS;
                 }
                 wait(NULL);
-                if (execl(name_exec_prg, name_exec_prg, NULL) < 0) {
+                /*if (execl(name_exec_prg, name_exec_prg, NULL) < 0) {
                     err(1, NULL);
-                }
-                free(path);
-                free(name_exec_prg);
+        `    }*/
+                free(exec_path);
+                free(name);
                 return EXIT_SUCCESS;
             }
+            free(exec_path);
+            free(name);   
         }
     }
     closedir(dir1);
+    printf("\nIm sure\n");
     return 0;
 }
