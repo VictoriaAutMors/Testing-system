90c477a0b74804786869d2777ee261a478835655
ghost171 <selevenko45@mail.ru>
2019-11-10 19:00:40 +0600

    test redirect 1 test to current program

--- src/test.c
+++ src/test.c
@@ -29,36 +29,39 @@ int main(int argc, char ** argv){
     char * right_way = NULL;
     char * directory_name = malloc(sizeof(char) * 2);
     directory_name[1] = '\0';
+    int fd;
+    char * filename = (char *)(malloc(sizeof(char) * 4096));
     while((entry_tmp = readdir(dir1)) != NULL){
         right_way = GetRightWay(entry_tmp->d_name);
         char * cmd[3] = {right_way, right_way, NULL};
         if(fork() == 0){
-            if(execvp(cmd[0], cmd) < 0){
-                err(1, NULL);
-            }
             chdir(strcat(getenv("PWD"), "/contest/tests"));
             directory_name[0] = entry_tmp->d_name[0];
             dir2 = opendir(directory_name);
             while ((entry_code = readdir(dir2)) != NULL)
             {
                 if(strstr(entry_code->d_name, ".dat") != NULL){
-                    char * filename = (char *)(malloc(sizeof(char) * 4096));
                     strcpy(filename, getenv("PWD"));
                     strcat(filename, "/");
                     strcat(filename, directory_name);
                     strcat(filename, "/");
                     strcat(filename, entry_code->d_name);
-                    int fd = open(filename, O_RDONLY, 0644);
+                    fd = open(filename, O_RDONLY, 0644);
                     dup2(fd, 0);
-                    close(fd);
-                    free(filename);
                     break;
                 }
             }
              
             chdir(strcat(getenv("PWD"), "/../.."));
+            puts(cmd[0]);
+            if(execvp(cmd[0], cmd) < 0){
+                err(1, NULL);
+            }
             return EXIT_SUCCESS;
+            close(fd);
+
         }
+
         wait(NULL);
     }
     return 0;
