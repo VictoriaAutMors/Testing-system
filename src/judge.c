fc4dfd619115559c64a7efe20bf036fd55cd0dd9
Timur Alikhanov <55821089+VictoriaAutMors@users.noreply.github.com>
2019-11-13 15:01:13 +0600

    Add files via upload

--- src/judge.c
+++ src/judge.c
@@ -9,75 +9,110 @@
 #include <err.h>
 #include <limits.h>
 #include <sys/wait.h>
-#include <math.h>
 
-char *get_prg_name(char *name, char *dname)
+/* functions that work with path */
+
+char *path_malloc(void)
 {
-    int len = strlen(dname);
-    if (name == NULL) {
+    char *path = (char *)malloc(PATH_MAX * sizeof(char));
+    if (path == NULL) {
         err(1, NULL);
     }
-    strncpy(name, dname, len - 2);
-    name[len - 1] = '\0';
-    name[len - 2] = '\0';
-    char * right_name = (char *)malloc(sizeof(char) * 4096);
-    strcpy(right_name, getenv("PWD"));
-    strcat(right_name, "/../../tmp/");
-    strcat(right_name, name);
-    strcpy(name, right_name);
-    free(right_name);
-    return name;
+    return path;
 }
 
-char *get_exec_path(char *exec_path, char *dname)
+char *get_exec_path(char *path, char *dname)
 {
-    //int len = strlen(dname);
-    if (exec_path == NULL) {
-        err(1, NULL);
-    }
-    strcpy(exec_path, getenv("PWD"));
-    strcat(exec_path, "/user1/");
-    strcat(exec_path, dname);
+    char *exec_path = path_malloc();
+    int len = strlen(dname);
+    strcpy(exec_path, path);
+    strcat(exec_path, "/../tmp/");
+    strncat(exec_path, dname, len - 2);
     return exec_path;
 }
 
-int main(){
-    pid_t pid;
-    char *name  = NULL, *exec_path = NULL;
-    struct dirent  *entry;
-    chdir(strcat(getenv("PWD"), "/../contest/code"));
-    DIR* dir1 = opendir("user1");
-    if (dir1 == NULL) {
+char *get_cmpl_path(char *path, char *wdir, char *dname)
+{
+    char *cmpl_path = path_malloc();
+    strcpy(cmpl_path, path);
+    strcat(cmpl_path, "/");
+    strcat(cmpl_path, wdir);
+    strcat(cmpl_path, "/");
+    strcat(cmpl_path, dname);
+    return cmpl_path;
+}
+
+/* functions that work with dirctories */
+
+void change_dir(char *name)
+{
+    char dir[PATH_MAX];
+    if (getcwd(dir, PATH_MAX) == NULL) {
         err(1, NULL);
     }
-    exec_path = (char *)malloc(sizeof(char) * 256);
-    name = (char *)malloc(sizeof(char) * 4096);
-    while ((entry = readdir(dir1)) != NULL) {
-        if (strstr(entry -> d_name, ".c") != NULL) {
-            name = get_prg_name(name, entry -> d_name);
-            exec_path = get_exec_path(exec_path, entry -> d_name);
-            if ((pid = fork()) == 0) {
-                if (fork() == 0) {
-                    char * cmd[7] = {"gcc", exec_path, "-o", name, "-Wall", "-Werror", NULL};
-                    if (execvp(cmd[0], cmd) < 0) {
-                        err(1, NULL);
-                    }
+    if (dir == NULL) {
+        err(1, NULL);
+    }
+    strcat(dir, name);
+    if (chdir(dir) < 0) {
+        err(1, name, NULL);
+    }
+}
+
+DIR *open_dir(char *name)
+{
+    DIR *dir = opendir(name);
+    if (dir == NULL) {
+        err(1, name, NULL);
+    }
+    return dir;
+}
+
+void get_cwd(char *wd)
+{
+    if (!getcwd(wd, PATH_MAX)) {
+        err(1, NULL);
+    }
+}
+
+/* other */
+
+void compile(char *cmpl_path, char *exec_path)
+{
+    char *cmd[8] = {"gcc", cmpl_path, "-o", exec_path, "-Wall", "-Werror", "-lm", NULL};
+    if (execvp(cmd[0], cmd) < 0) {
+        err(1, NULL);
+    }
+}
+
+int main(void){
+    pid_t pid;
+    DIR *pdir, *cdir;
+    struct dirent  *pcat, *ccat;
+    char *cmpl_path = NULL, *exec_path = NULL;
+    char code_dir[PATH_MAX], src_dir[PATH_MAX];
+    get_cwd(src_dir);
+    change_dir("/../contest");
+    pdir = open_dir("code");
+    change_dir("/code");
+    get_cwd(code_dir);
+    while ((pcat = readdir(pdir)) != NULL) {
+        cdir = open_dir(pcat -> d_name);
+        while ((ccat = readdir(cdir)) != NULL) {
+            if (strstr(ccat -> d_name, ".c") != NULL) {
+                cmpl_path = get_cmpl_path(code_dir, pcat -> d_name, ccat -> d_name);
+                exec_path = get_exec_path(src_dir, ccat -> d_name);
+                if ((pid = fork()) == 0) {
+                    compile(cmpl_path, exec_path);
                     return EXIT_SUCCESS;
                 }
                 wait(NULL);
-                /*if (execl(name_exec_prg, name_exec_prg, NULL) < 0) {
-                    err(1, NULL);
-        `    }*/
-                //free(exec_path);
-                //free(name);
-                return EXIT_SUCCESS;
+                free(cmpl_path);
+                free(exec_path);
             }
-            wait(NULL);
-            //free(name);   
         }
+        closedir(cdir);
     }
-    free(exec_path);
-    free(name);
-    closedir(dir1);
+    closedir(pdir);
     return 0;
 }
