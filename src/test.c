05406256db877d5eb5a2b570c15068ce8561dc54
Timur Alikhanov <55821089+VictoriaAutMors@users.noreply.github.com>
2019-11-19 10:06:42 +0600

    Add files via upload

--- src/test.c
+++ src/test.c
@@ -12,6 +12,9 @@
 #include <sys/wait.h>
 #include <math.h>
 
+#define TRUE 1
+#define FALSE 0
+
 int genc = 5;
 
 char * GetRightWay(char * name){
@@ -31,7 +34,7 @@ int count_dat(DIR * dir){
             i++;
         }
     }
-    return i; 
+    return i;
 }
 
 void ch_dir(char *dir)
@@ -50,23 +53,17 @@ DIR *open_dir(char *dir)
     return tmp;
 }
 
-DIR *opentest(void)
-{
-    ch_dir("contest");
-    return open_dir("tests");
-}
-
-struct dirent *ctest(char *name)
+int is_test_legit(char *name)
 {
     struct dirent *ctest;
     DIR * tests_dir;
-    tests_dir = opendir("tests");
-    while (ctest = readdir(tests_dir) != NULL) {
+    tests_dir = open_dir("tests");
+    while ((ctest = readdir(tests_dir)) != NULL) {
         if (strcmp(ctest -> d_name, name) == 0) {
-            return ctest;
+            return TRUE;
         }
     }
-    return NULL;
+    return FALSE;
 }
 
 void write_result(char *name, char *result)
@@ -94,91 +91,91 @@ char *prepare_exec(char *name)
     strcat(exec, name);
     ch_dir("..");
     ch_dir("..");
+    ch_dir("..");
     ch_dir("tmp");
     return exec;
 }
 
+ssize_t fd_ropen(char *name)
+{
+    ssize_t fd = open(name, O_RDONLY);
+    if (fd < 0) {
+        err(1, NULL);
+    }
+    return fd;
+}
+
+char *fill_from_fd(ssize_t fd)
+{
+    int i = 0;
+    char ch, *buff = NULL, *tmp = NULL;
+    while (read(fd, &ch, 1)) {
+        buff[i] = ch;
+        i++;
+        tmp = (char *)realloc(buff, (i + 1) * sizeof(char));
+        if (tmp == NULL) {
+            err(1, NULL);
+        }
+        buff = tmp;
+    }
+    return buff;
+}
+
 int main(void)
 {
-    struct dirent *entry_bin, *entry_tests, *entry_test;
-    DIR *dir_bin, *dir_tests;
+    struct dirent *entry_bin;
+    DIR *dir_bin;
     pid_t pid;
-    ssize_t fd;
-    char test_name[8];
+    ssize_t fd_data, fd_tmp, fd_ans;
+    char test_name[8], ans_name[8], *buff_answer, *buff_test;
+    int i, flag = 0;
     ch_dir("..");
-    char ans_name[8];
     dir_bin = open_dir("tmp");
-    dir_tests = opentest();
+    ch_dir("contest");
     while((entry_bin = readdir(dir_bin)) != NULL){
         if((pid = fork()) == 0) {
-            entry_tests = ctest(entry_bin -> d_name);
-            if (entry_tests == NULL) {
+            if (is_test_legit(entry_bin -> d_name) == FALSE) {
                 ch_dir("..");
                 ch_dir("..");
                 ch_dir("tmp");
                 write_result(entry_bin->d_name, "-");
                 return EXIT_SUCCESS;
             }
-            int flag = 0;
-            for (int i = 1; i <= genc; i++) {
+            for (i = 1; i <= genc; i++) {
                 ch_dir(entry_bin -> d_name);
                 sprintf(test_name, "03%d.dat", i);
-                fd = open(test_name, O_RDONLY);
-                if (fd < 0) {
-                    err(1, NULL);
-                }
-                dup2(STDIN_FILENO, fd);
-                char *exec = prepare_exec(entry_bin -> d_name);
-                ssize_t fdd = open("anss", O_WRONLY | O_CREAT | O_TRUNC, 0755);
-                dup2(STDOUT_FILENO, fdd);
-
+                sprintf(ans_name, "03%d.ans", i);
+                fd_data = fd_ropen(test_name);
+                fd_ans = fd_ropen(ans_name);
+                dup2(STDIN_FILENO, fd_data);
+                fd_tmp = open("answer", O_WRONLY | O_CREAT | O_TRUNC, 0755);
                 if (fork() == 0) {
+                    char *exec = prepare_exec(entry_bin -> d_name);
+                    dup2(STDOUT_FILENO, fd_tmp);
                     if (execlp(exec, exec, NULL) < 0) {
                         write_result(entry_bin -> d_name, "-");
-                        close(fdd);
+                        close(fd_tmp);
                         return EXIT_FAILURE;
                     }
+                    return EXIT_SUCCESS;
                 }
                 wait(NULL);
-                char * buf_answer = malloc(sizeof(char));  //array of program's answer
-                char * buf_test = malloc(sizeof(char));    //array of test's content
-                char ch = EOF;
-                int j = 0;
-
-                while(read(fdd, &ch, 1)){
-                    buf_answer[j] = ch;
-                    j++;
-                    buf_answer = realloc(buf_answer, sizeof(char) * (j + 1));
-                }
-                close(fdd);
-                ch_dir("..");
-                ch_dir("contest");
-                ch_dir("tests");
-                ch_dir(entry_bin->d_name);
-                sprintf(ans_name, "03%d.ans", i);
-                int fddd = open(ans_name, O_RDONLY, 0755);
-                j = 0;
-                while (read(fddd, &ch, 1))
-                {
-                    buf_test[j] = ch;
-                    j++;
-                    buf_test = realloc(buf_test, sizeof(char) * 1); 
-                }
-                close(fddd);
-                if(strstr(buf_test, buf_answer) != 0){
+                buff_test = fill_from_fd(fd_tmp);
+                close(fd_tmp);
+                buff_answer = fill_from_fd(fd_ans);
+                close(fd_ans);
+                if(strstr(buff_test, buff_answer) != 0){
                     flag = 1;
                     break;
                 }
-                return EXIT_SUCCESS;
             }
-                if(flag){
-                    write_result(entry_bin->d_name, "-");
-                }
-                else{
-                    write_result(entry_bin->d_name, "+");
-                }
- 
-                return EXIT_SUCCESS;
+            if (flag) {
+                write_result(entry_bin -> d_name, "-");
+            }
+            else {
+                write_result(entry_bin -> d_name, "+");
+            }
+            return EXIT_SUCCESS;
         }
         wait(NULL);
     }
