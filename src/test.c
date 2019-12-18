caa1603e67b15557027beec821435a359f16aa67
Timur Alikhanov <55821089+VictoriaAutMors@users.noreply.github.com>
2019-12-18 22:41:04 +0600

    Add files via upload

--- src/test.c
+++ src/test.c
@@ -43,23 +43,11 @@ DIR *open_dir(char *dir)
     return tmp;
 }
 
-int is_test_legit(char *name)
-{
-    struct dirent *ctest;
-    DIR * tests_dir;
-    tests_dir = open_dir("tests");
-    while ((ctest = readdir(tests_dir)) != NULL) {
-        if (strcmp(ctest -> d_name, name) == 0) {
-            closedir(tests_dir);
-            return TRUE;
-        }
-    }
-    closedir(tests_dir);
-    return FALSE;
-}
+/* functions that work with log files */
 
+/* write error, date and time in log file */
 void write_in_log(char *error) {
-    FILE *fl = fopen("log.txt", "a");
+    FILE *fl = fopen("test_logs.txt", "a+");
     time_t rawtime;
     struct tm *timeinfo;
     if (time(&rawtime) < 0) {
@@ -70,24 +58,15 @@ void write_in_log(char *error) {
     fclose(fl);
 }
 
-void logger(char *string, char *path) {
-    ch_dir("../log");
+/* change dir to log and go back to previous directory*/
+void logger(char *string, char *old_dir) {
+    ch_dir("../logs");
     write_in_log(string);
     ch_dir("..");
-    ch_dir(path);
+    ch_dir(old_dir);
 }
 
-char *prepare_exec(char *name)
-{
-    char path[PATH_MAX];
-    char *exec = (char *)malloc(4 * sizeof(char));
-    strcpy(exec, name);
-    if (getcwd(path, PATH_MAX) == NULL) {
-        err(1, "getcwd error");
-    }
-    setenv("PWD", path, TRUE);
-    return exec;
-}
+/* functions that work with FILEs and file descriptors*/
 
 ssize_t fd_open(char *name) {
     ssize_t fd = open(name, O_RDWR | O_CREAT, 0755);
@@ -97,8 +76,8 @@ ssize_t fd_open(char *name) {
     return fd;
 }
 
-void close_fd(int fd1, ...)
-{
+/* closes n's file descriptors with va_list */
+void fd_close(int fd1, ...) {
     va_list fd;
     ssize_t i;
     va_start(fd, fd1);
@@ -108,14 +87,95 @@ void close_fd(int fd1, ...)
     va_end(fd);
 }
 
-FILE *fl_rfopen(char *name)
-{
+FILE *fl_rfopen(char *name) {
     FILE *fl = fopen(name, "r");
     if (fl == NULL) {
-        err(1, name, NULL);
+        err(ERR, name, NULL);
     }
     return fl;
 }
+
+/* functions that obtain information from config files */
+
+/* Read line from file. it returns the whole line */
+char *read_line(FILE *fl) {
+    int len = 0;
+    char *line = (char *)malloc(LINE_MAX);
+    if (line == NULL) {
+        err(1, NULL);
+    }
+    if (fgets(line, LINE_MAX, fl) == NULL) {
+        free(line);
+        return NULL;
+    }
+    len = strlen(line);
+    line[len - 1] = '\0';
+    return line;
+}
+
+/* Get number from line that ends with ';'. It returns that number*/
+int get_num(char *line) {
+    int num = 0, i = 0;
+    do {
+        if (line[i] >= '0' && line[i] <= '9') {
+            num *= 10;
+            num += line[i] - '0';
+        }
+        i++;
+    } while (line[i] != ';');
+    return num;
+}
+
+/* gets type of checking */
+int get_check_type(char *line) {
+    int i = 0, ans = 0;
+    char error[1024];
+    while(line[i] != ';') {
+        i++;
+    }
+    if (line[i - 1] == 'i') {
+        ans = INT;
+    } else if (line[i - 1] == 'b') {
+        ans = CHAR;
+    } else {
+        sprintf(error, "no information about check type");
+        logger(error, "contest");
+        exit(1);
+    }
+    return ans;
+}
+
+/* functions open "file" and search line with "name". 
+Flag defines what information is needed*/
+int get_info(char *file, char *name, int flag) {
+    FILE *fl = fopen(file, "r");
+    char *line, *tmp = NULL, error[1024];
+    int ans;
+    if (fl == NULL) {
+        sprintf(error, "failed to open %s ", file);
+        logger(error, "contest");
+        exit(1);
+    }
+    while ((line = read_line(fl)) != NULL) {
+        tmp = strstr(line, name);
+        if (tmp) {
+            if (flag == COUNT) {
+                ans = get_num(tmp);
+            } else {
+                ans = get_check_type(tmp);
+            }
+            free(line);
+            fclose(fl);
+            return ans;
+        }
+        free(line);
+    }
+    logger("failed to get information from config files", "contest");
+    fclose(fl);
+    exit(1);
+}
+
+/* functions that compare 2 FILEs */
 
 int cmp_byte(FILE *fl1, FILE *fl2)
 {
@@ -152,92 +212,12 @@ int cmp_int(FILE *fl1, FILE *fl2){
     return 0;
 }
 
-char *read_line(FILE *fl) {
-    int len = 0;
-    char *line = (char *)malloc(LINE_MAX);
-    if (line == NULL) {
-        err(1, NULL);
-    }
-    if (fgets(line, LINE_MAX, fl) == NULL) {
-        free(line);
-        return NULL;
-    }
-    len = strlen(line);
-    line[len - 1] = '\0';
-    return line;
-}
-
-int get_num(char *line) {
-    int num = 0, i = 0;
-    do {
-        if (line[i] >= '0' && line[i] <= '9') {
-            num *= 10;
-            num += line[i] - '0';
-        }
-        i++;
-    } while (line[i] != ';');
-    return num;
-}
-
-int get_check_type(char *line) {
-    int i = 0, ans = 0;
-    char *error = NULL;
-    while(line[i] != ';') {
-        i++;
-    }
-    if (line[i - 1] == 'i') {
-        ans = INT;
-    } else if (line[i - 1] == 'b') {
-        ans = CHAR;
-    } else {
-        sprintf(error, "no information about check type");
-        logger(error, "contest");
-        exit(1);
-    }
-    return ans;
-}
-
-int get_info(char *file, char *name, int flag) {
-    FILE *fl = fopen(file, "r");
-    char *line, *tmp = NULL;
-    int ans;
-    if (fl == NULL) {
-        sprintf(tmp, "failed to open %s ", file);
-        logger(tmp, "contest");
-        exit(1);
-    }
-    while ((line = read_line(fl)) != NULL) {
-        tmp = strstr(line, name);
-        if (tmp) {
-            if (flag == COUNT) {
-                ans = get_num(tmp);
-            } else {
-                ans = get_check_type(tmp);
-            }
-            free(line);
-            fclose(fl);
-            return ans;
-        }
-        free(line);
-    }
-    logger("failed to get information from config files", "contest");
-    fclose(fl);
-    exit(1);
-}
-
-void handler(void) {
-    putchar('-');
-    logger("program running for too long", "contest");
-    kill(child, SIGKILL);
-}
-
+/* it checks return status of program. After that calls checkers*/
 int check_files(int status, FILE *fl1, FILE *fl2, char *task_name) {
     int check;
     ch_dir("../contest/");
     check = get_info("checker.cfg", task_name, CHECK_TYPE);
-    ch_dir("tests");
-    ch_dir(task_name);
-    ch_dir("../../../tmp");
+    ch_dir("../tmp");
     if (WEXITSTATUS(status) != 0) {
         return 1;
     }
@@ -247,71 +227,125 @@ int check_files(int status, FILE *fl1, FILE *fl2, char *task_name) {
     return cmp_int(fl1, fl2);
 }
 
-int main(void) {
+/* additional checking fucntions */
+
+/* checks all compiled tasks' name with tasks' name in contest */
+int is_test_legit(char *name)
+{
+    struct dirent *ctest;
+    DIR * tests_dir;
+    tests_dir = open_dir("tests");
+    while ((ctest = readdir(tests_dir)) != NULL) {
+        if (strcmp(ctest -> d_name, name) == 0) {
+            closedir(tests_dir);
+            return TRUE;
+        }
+    }
+    closedir(tests_dir);
+    return FALSE;
+}
+
+/* prepare argument vector and sets enviroment variable for exec */
+char *prepare_exec(char *name)
+{
+    char path[PATH_MAX];
+    char *exec = (char *)malloc(4 * sizeof(char));
+    strcpy(exec, name);
+    if (getcwd(path, PATH_MAX) == NULL) {
+        err(1, "getcwd error");
+    }
+    setenv("PWD", path, TRUE);
+    return exec;
+}
+
+/* Timer. If program works more than 5 seconds, it will be suspended*/
+void handler(void) {
+    putchar('-');
+    logger("program running for too long", "contest");
+    kill(child, SIGKILL);
+}
+
+/* check all tests for task in loop */
+int check_all_tasks(int count, char *test_name, DIR *dir, int time) {
+    FILE *fl_ans, *fl_tmp;
+    ssize_t fd_data, fd_tmp;
+    int flag, status;
+    char data_name[8], ans_name[8], error[1024];
+    for (int i = 1; i <= count; i++) {
+        flag = 0;
+        ch_dir("tests");
+        ch_dir(test_name);
+        sprintf(data_name, "%03d.dat", i);
+        sprintf(ans_name, "%03d.ans", i);
+        fd_data = fd_open(data_name);
+        fl_ans = fl_rfopen(ans_name);
+        ch_dir("../../../tmp");
+        fd_tmp = open("answer", O_RDWR | O_TRUNC | O_CREAT, 0755);
+        alarm(time);
+        if ((child = fork()) == 0) {
+            char *exec = prepare_exec(test_name);
+            dup2(fd_data, STDIN_FILENO);
+            dup2(fd_tmp, STDOUT_FILENO);
+            if (execl(exec, exec, NULL) < 0) {
+                sprintf(error, "%s- failed on task %d", test_name, i);
+                logger(error, "tmp");
+                fd_close(fd_tmp, fd_data);
+                closedir(dir);
+                exit(ERR);
+            }
+            fd_close(fd_tmp, fd_data);
+            closedir(dir);
+            return EXIT_SUCCESS;
+        }
+        waitpid(child, &status, 0);
+        alarm(0);
+        fd_close(fd_data);
+        fl_tmp = fl_rfopen("answer");
+        flag = check_files(status, fl_ans, fl_tmp, test_name);
+        if (flag == 0) {
+            putchar('+');
+        } else {
+            putchar('-');
+            sprintf(error, "%s:Wrong answer on test %d", test_name, i);
+            logger(error, "tmp");
+        }
+        fclose(fl_tmp);
+        fclose(fl_ans);
+        ch_dir("../contest/");
+    }
+    putchar('\n');
+    return EXIT_SUCCESS;
+}
+
+int test(void) {
     struct dirent *entry_bin;
     DIR *dir_bin;
-    int status, i, test_count = 0, flag = 0;
-    FILE *fl_tmp, *fl_ans;
-    ssize_t fd_tmp, fd_data;
-    char test_name[8], ans_name[8], error[1024];
-    signal(SIGALRM, (void (*)(int))handler);
+    int time, test_count = 0;
+    char error[1024];
     ch_dir("..");
     dir_bin = open_dir("tmp");
     ch_dir("contest");
     while((entry_bin = readdir(dir_bin)) != NULL) {
         if(!strstr(entry_bin -> d_name, ".") && strcmp(entry_bin -> d_name,
-                                                    "answer")) {
+                                                                "answer")) {
             printf("%s ", entry_bin -> d_name);
             if (is_test_legit(entry_bin -> d_name) == FALSE) {
                 putchar('-');
                 sprintf(error, "%s- no test; ", entry_bin -> d_name);
                 logger(error, "contest");
-                return EXIT_SUCCESS;
-            }
+            } else {
             test_count = get_info("global.cfg", entry_bin -> d_name, COUNT);
-            for (i = 1; i <= test_count; i++) {
-                flag = 0;
-                ch_dir("tests");
-                ch_dir(entry_bin -> d_name);
-                sprintf(test_name, "%03d.dat", i);
-                sprintf(ans_name, "%03d.ans", i);
-                fd_data = fd_open(test_name);
-                fl_ans = fl_rfopen(ans_name);
-                ch_dir("../../../tmp");
-                fd_tmp = open("answer", O_RDWR | O_TRUNC | O_CREAT, 0755);
-                alarm(5);
-                if ((child = fork()) == 0) {
-                    char *exec = prepare_exec(entry_bin -> d_name);
-                    dup2(fd_data, STDIN_FILENO);
-                    dup2(fd_tmp, STDOUT_FILENO);
-                    if (execl(exec, exec, NULL) < 0) {
-                        putchar('-');
-                        sprintf(error, "%s- failed on task %d", entry_bin -> d_name, i);
-                        logger(error, "tmp");
-                    }
-                    close_fd(fd_tmp, fd_data);
-                    closedir(dir_bin);
-                    return EXIT_SUCCESS;
-                }
-                waitpid(child, &status, 0);
-                alarm(0);
-                close_fd(fd_tmp, fd_data);
-                fl_tmp = fl_rfopen("answer");
-                flag = check_files(status, fl_ans, fl_tmp, entry_bin -> d_name);
-                if (flag == 0) {
-                    putchar('+');
-                } else {
-                    putchar('-');
-                    sprintf(error, "%s:Wrong answer on test %d", entry_bin->d_name, i);
-                    logger(error, "tmp");
-                }
-                fclose(fl_ans);
-                fclose(fl_tmp);
-                ch_dir("../contest/");
+            time = get_info("global.cfg", "task_time", COUNT);
+            check_all_tasks(test_count, entry_bin -> d_name, dir_bin, time);
             }
         }
-        putchar('\n');
     }
     closedir(dir_bin);
+    return EXIT_SUCCESS;
+}
+
+int main(void) {
+    signal(SIGALRM, (void (*)(int))handler);
+    test();
     return EXIT_SUCCESS;
 }
